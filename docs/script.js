a173ecb63cf046559b46e457067a0e07dd921907
bbostandzhiev-qb <bbostandzhiev@quickbase.com>
2021-01-20 12:56:22 +0200

    Initial

--- docs/script.js
+++ docs/script.js
@@ -1,37 +1,36 @@
 // Synthesis
 var synth = window.speechSynthesis;
 // Recognition
-var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
-var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
-var grammar = '#JSGF V1.0;'
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
+var grammar = "#JSGF V1.0;";
 var recognition = new SpeechRecognition();
 var speechRecognitionList = new SpeechGrammarList();
 speechRecognitionList.addFromString(grammar, 1);
 recognition.grammars = speechRecognitionList;
 recognition.continuous = false;
-recognition.lang = 'en-US';
+recognition.lang = "en-US";
 recognition.interimResults = false;
 recognition.maxAlternatives = 1;
-
 
 const urlParams = new URLSearchParams(window.location.search);
 var hook = urlParams.get("hook");
 
 var listening = false;
-var mic = document.querySelector('#mic');
-var resultText = document.querySelector('#result');
-var url = 'https://5e8c8104e61fbd00164aed46.mockapi.io/pipelines/speak';
+var mic = document.querySelector("#mic");
+var resultText = document.querySelector("#result");
+var url = "https://5e8c8104e61fbd00164aed46.mockapi.io/pipelines/speak";
 
-function speak(text){
+function speak(text) {
   if (synth.speaking) {
-    console.error('speechSynthesis.speaking');
+    console.error("speechSynthesis.speaking");
     return;
   }
   var utterThis = new SpeechSynthesisUtterance(text);
   synth.speak(utterThis);
 }
 
-function appendParagraph(text, type='speech') {
+function appendParagraph(text, type = "speech") {
   var node = document.createElement("P");
   var textnode = document.createTextNode(text);
   node.appendChild(textnode);
@@ -40,69 +39,75 @@ function appendParagraph(text, type='speech') {
 }
 
 function processResponse(data) {
-  var respond = 'No response'
-  if ('speech_synthesize' in data) {
+  var respond = "No response";
+  if ("speech_synthesize" in data) {
     respond = data.speech_synthesize;
   }
 
   speak(respond);
-  appendParagraph(respond, 'response');
+  appendParagraph(respond, "response");
 }
 
 async function triggerHook(payload = {}) {
-  fetch(hook || url, {
-    method: 'POST',
-    mode: hook ? 'no-cors': 'cors',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(payload)
-  }).then(response => {
-    if (!response.ok) {
-      throw new Error('Response was not ok');
-    }
-    return response.json();
-  })
-  .then(data => {
-    processResponse(data);
-  })
-  .catch(error => {
-    speak(error);
-    appendParagraph(error, 'error');
-  });
+  const prompt = window.confirm(`Send ${payload.speech_recognised} ?`);
+  if (prompt) {
+    fetch(hook || url, {
+      method: "POST",
+      mode: hook ? "no-cors" : "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        processResponse(data);
+      })
+      .catch((error) => {
+        speak(error);
+        appendParagraph(error, "error");
+      });
+  }
+  else{
+    appendParagraph("Not Sent");
+  }
 }
 
-mic.onclick = function() {
+mic.onclick = function () {
   if (listening === false) {
     listening = true;
-    mic.classList.add('active');
+    mic.classList.add("active");
     recognition.start();
   } else {
     recognition.stop();
     listening = false;
-    mic.classList.remove('active');
+    mic.classList.remove("active");
   }
-}
+};
 
-recognition.onresult = function(event) {
+recognition.onresult = function (event) {
   var lastResult = event.results[event.results.length - 1][0];
   var result = lastResult.transcript.toLowerCase().trim();
 
   appendParagraph(result);
   triggerHook({ speech_recognised: result });
-}
+};
 
-recognition.onspeechend = function() {
+recognition.onspeechend = function () {
   listening = false;
   recognition.stop();
-  mic.classList.remove('active');
-}
+  mic.classList.remove("active");
+};
 
-recognition.onnomatch = function(event) {
-  appendParagraph("I didn't recognise this.", 'error');
-}
+recognition.onnomatch = function (event) {
+  appendParagraph("I didn't recognise this.", "error");
+};
 
-recognition.onerror = function(event) {
-  appendParagraph('Error occurred in recognition: ' + event.error, 'error');
-}
-
+recognition.onerror = function (event) {
+  appendParagraph("Error occurred in recognition: " + event.error, "error");
+};
