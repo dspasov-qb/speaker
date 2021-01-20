901da45fbe4e1c6a5d9cecae103b55a6ab4adc67
bbostandzhiev-qb <bbostandzhiev@quickbase.com>
2021-01-20 16:33:31 +0200

    W.I.P.

--- docs/script.js
+++ docs/script.js
@@ -46,16 +46,16 @@ function toggleRecognition(switchOn) {
 
 mic.onclick = toggleRecognition;
 
-function speak(text, listenOnEnd=false) {
+function speak(text, listenOnEnd = false) {
   if (synth.speaking) {
     console.error("speechSynthesis.speaking");
     return;
   }
   var utterThis = new SpeechSynthesisUtterance(text);
   if (listenOnEnd) {
-    utterThis.onend= function(event) {
+    utterThis.onend = function (event) {
       toggleRecognition();
-    }
+    };
   }
   synth.speak(utterThis);
 }
@@ -68,7 +68,7 @@ function appendParagraph(text, type = "speech") {
   resultText.appendChild(node);
 }
 
-function speakAndWrite(string, type="response", listenOnEnd=false) {
+function speakAndWrite(string, type = "response", listenOnEnd = false) {
   speak(string, listenOnEnd);
   appendParagraph(string, type);
 }
@@ -85,60 +85,57 @@ function processResponse(data) {
   speakAndWrite(respond);
 }
 
-function sendRequest(payload) {
-  fetch(hookUrl.href, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(payload),
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Response was not ok");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      processResponse(data);
-    })
-    .catch((error) => {
-      speakAndWrite(respond, "error");
-    });
-}
-
-var prevPayload = false;
 async function triggerHook(payload = {}) {
   if (hookUrl) {
-    if (!prevPayload) {
-      prevPayload = payload;
-      speakAndWrite(
-        `Do you want to send, "${payload.transcript}"?`,
-        'response',
-        true
-      );
-    } else {
-      const answer = payload.transcript;
-      if (answer === "yes") {
-        sendRequest(prevPayload);
-      
-      } else {
-        speakAndWrite("Did not send");
-      }
-
-      prevPayload = false;
-    }
+    fetch(hookUrl.href, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        processResponse(data);
+      })
+      .catch((error) => {
+        speakAndWrite(respond, "error");
+      });
   } else {
-    speakAndWrite('Hook is not a valid url', 'error');
+    speakAndWrite("Hook is not a valid url", "error");
   }
 }
 
+var prevPayload = false;
 recognition.onresult = function (event) {
   var lastResult = event.results[event.results.length - 1][0];
   var result = lastResult.transcript.toLowerCase().trim();
 
   appendParagraph(result);
-  triggerHook({ transcript: result, conversation_id: conversation_id });
+
+  const payload = { transcript: result, conversation_id: conversation_id };
+  if (!prevPayload) {
+    prevPayload = payload;
+    speakAndWrite(
+      `Do you want to send, "${payload.transcript}"?`,
+      "response",
+      true
+    );
+  } else {
+    const answer = payload.transcript;
+    if (answer === "yes") {
+      triggerHook(payload);
+    } else {
+      speakAndWrite("Did not send");
+    }
+
+    prevPayload = false;
+  }
 };
 
 recognition.onspeechend = function () {
