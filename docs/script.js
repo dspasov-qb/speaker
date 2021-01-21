da2c4107c42b4006d7b8c0e5c0484dcbb37cae7a
bbostandzhiev-qb <bbostandzhiev@quickbase.com>
2021-01-21 14:05:21 +0200

    added dynamic manifest

--- docs/script.js
+++ docs/script.js
@@ -19,6 +19,30 @@ const urlParams = new URLSearchParams(window.location.search);
 var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
 var wavesurfer, context, processor;
 
+var myDynamicManifest = {
+  "name": "Speaker",
+  "short_name": "Speaker",
+  "description": "Client for Pipelines Speech channel",
+  "lang": "en-US",
+  "start_url": `https://dspasov-qb.github.io/speaker/?hook=${urlParams.get("hook")}`,
+  "display": "standalone",
+  "theme_color": "#1c2c17",
+  "icons": [
+    {
+      "src": "images/speech.svg",
+      "sizes": "48x48 192x192 512x512",
+      "type": "image/svg+xml"
+    }
+  ],
+  "background_color": "#111111"
+}
+
+const stringManifest = JSON.stringify(myDynamicManifest);
+const blob = new Blob([stringManifest], {type: 'application/json'});
+const manifestURL = URL.createObjectURL(blob);
+document.querySelector('#my-manifest-placeholder').setAttribute('href', manifestURL);
+document.querySelector('#msapplication-starturl').setAttribute('content', `https://dspasov-qb.github.io/speaker/?hook=${urlParams.get("hook")}`);
+
 // Init & load
 document.addEventListener("DOMContentLoaded", function () {
   var micBtn = document.querySelector("#mic");
@@ -73,7 +97,6 @@ document.addEventListener("DOMContentLoaded", function () {
         wavesurfer.microphone.start();
       }
     }
-
 
     // сад
     var hookUrl;
@@ -134,127 +157,127 @@ document.addEventListener("DOMContentLoaded", function () {
       speak(string, listenOnEnd);
       appendParagraph(string, type);
     }
-var hookUrl;
-var prevTranscript = null;
-var listening = false;
-var conversation_id = null;
-var mic = document.querySelector("#mic");
-var resultText = document.querySelector("#result");
+    var hookUrl;
+    var prevTranscript = null;
+    var listening = false;
+    var conversation_id = null;
+    var mic = document.querySelector("#mic");
+    var resultText = document.querySelector("#result");
 
-// Hook url
-try {
-  var hookUrl = new URL(window.atob(urlParams.get("hook")));
-  if (!(hookUrl.protocol === "http:" || hookUrl.protocol === "https:")) {
-    hookUrl = undefined;
-    console.warn("Hook is not a valid url");
-  }
-} catch (e) {
-  console.warn(e);
-}
-
-function toggleRecognition(switchOn) {
-  if (listening === false) {
-    listening = true;
-    mic.classList.add("active");
-    recognition.start();
-  } else {
-    recognition.stop();
-    listening = false;
-    mic.classList.remove("active");
-  }
-}
-
-mic.onclick = toggleRecognition;
-
-function speak(text, listenOnEnd = false) {
-  if (synth.speaking) {
-    console.error("speechSynthesis.speaking");
-    return;
-  }
-  var utterThis = new SpeechSynthesisUtterance(text);
-  if (listenOnEnd) {
-    utterThis.onend = function (event) {
-      toggleRecognition();
-    };
-  }
-  synth.speak(utterThis);
-}
-
-function appendParagraph(text, type = "speech") {
-  var node = document.createElement("P");
-  var textnode = document.createTextNode(text);
-  node.appendChild(textnode);
-  node.classList.add(type);
-  resultText.appendChild(node);
-}
-
-function speakAndWrite(string, type = "response", listenOnEnd = false) {
-  speak(string, listenOnEnd);
-  appendParagraph(string, type);
-}
-
-function processResponse(data) {
-  var respond = "No response";
-  if ("speech_synthesise" in data) {
-    respond = data.speech_synthesise;
-  }
-  if ("conversation_id" in data) {
-    conversation_id = data.conversation_id;
-  }
-
-  speakAndWrite(respond);
-}
-
-async function triggerHook(payload = {}) {
-  if (hookUrl) {
-    fetch(hookUrl.href, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(payload),
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Response was not ok");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        processResponse(data);
-      })
-      .catch((error) => {
-        speakAndWrite(respond, "error");
-      });
-  } else {
-    speakAndWrite("Hook is not a valid url", "error");
-  }
-}
-
-function confirmTranscript(transcript) {
-  if (!prevTranscript) {
-    prevTranscript = transcript;
-    speakAndWrite(
-      `Do you want to send "${transcript}"?`,
-      "response",
-      true
-    );
-  } else {
-    if (['yes', 'yes please', 'ok', 'send', 'yeah', 'hell yeah'].includes(transcript)) {
-      triggerHook({ transcript: prevTranscript, conversation_id });
-    } else {
-      speakAndWrite("Didn't send it.");
+    // Hook url
+    try {
+      var hookUrl = new URL(window.atob(urlParams.get("hook")));
+      if (!(hookUrl.protocol === "http:" || hookUrl.protocol === "https:")) {
+        hookUrl = undefined;
+        console.warn("Hook is not a valid url");
+      }
+    } catch (e) {
+      console.warn(e);
     }
-    prevTranscript = null;
-  }
-}
 
-recognition.onresult = function (event) {
-  var lastResult = event.results[event.results.length - 1][0];
-  var result = lastResult.transcript.toLowerCase().trim();
-  appendParagraph(result);
-  confirmTranscript(result)
-};
+    function toggleRecognition(switchOn) {
+      if (listening === false) {
+        listening = true;
+        mic.classList.add("active");
+        recognition.start();
+      } else {
+        recognition.stop();
+        listening = false;
+        mic.classList.remove("active");
+      }
+    }
+
+    mic.onclick = toggleRecognition;
+
+    function speak(text, listenOnEnd = false) {
+      if (synth.speaking) {
+        console.error("speechSynthesis.speaking");
+        return;
+      }
+      var utterThis = new SpeechSynthesisUtterance(text);
+      if (listenOnEnd) {
+        utterThis.onend = function (event) {
+          toggleRecognition();
+        };
+      }
+      synth.speak(utterThis);
+    }
+
+    function appendParagraph(text, type = "speech") {
+      var node = document.createElement("P");
+      var textnode = document.createTextNode(text);
+      node.appendChild(textnode);
+      node.classList.add(type);
+      resultText.appendChild(node);
+    }
+
+    function speakAndWrite(string, type = "response", listenOnEnd = false) {
+      speak(string, listenOnEnd);
+      appendParagraph(string, type);
+    }
+
+    function processResponse(data) {
+      var respond = "No response";
+      if ("speech_synthesise" in data) {
+        respond = data.speech_synthesise;
+      }
+      if ("conversation_id" in data) {
+        conversation_id = data.conversation_id;
+      }
+
+      speakAndWrite(respond);
+    }
+
+    async function triggerHook(payload = {}) {
+      if (hookUrl) {
+        fetch(hookUrl.href, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(payload),
+        })
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error("Response was not ok");
+            }
+            return response.json();
+          })
+          .then((data) => {
+            processResponse(data);
+          })
+          .catch((error) => {
+            speakAndWrite(respond, "error");
+          });
+      } else {
+        speakAndWrite("Hook is not a valid url", "error");
+      }
+    }
+
+    function confirmTranscript(transcript) {
+      if (!prevTranscript) {
+        prevTranscript = transcript;
+        speakAndWrite(`Do you want to send "${transcript}"?`, "response", true);
+      } else {
+        if (
+          ["yes", "yes please", "ok", "send", "yeah", "hell yeah"].includes(
+            transcript
+          )
+        ) {
+          triggerHook({ transcript: prevTranscript, conversation_id });
+        } else {
+          speakAndWrite("Didn't send it.");
+        }
+        prevTranscript = null;
+      }
+    }
+
+    recognition.onresult = function (event) {
+      var lastResult = event.results[event.results.length - 1][0];
+      var result = lastResult.transcript.toLowerCase().trim();
+      appendParagraph(result);
+      confirmTranscript(result);
+    };
 
     function processResponse(data) {
       var respond = "No response";
